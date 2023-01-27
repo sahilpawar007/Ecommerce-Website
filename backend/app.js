d8ddb96607eedb42d60dae6fcb9dac69ff7784d8
Sahil Pawar <sahilpawar0325@gmail.com>
2023-01-27 20:32:13 +0530

    2nd commit

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,12 @@
 const express = require('exrpess');
 const app = express();
 
+app.use(express.json());
+
+// ROUTE Imports
+
+const product = require('./routes/productRoute');
+
+app.use('/api/v1',product);
 
 module.exports = app;
