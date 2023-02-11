aa1ff5a0fb467c61ef8ee9184b64f60a8375bd1e
Sahil Pawar <sahilpawar0325@gmail.com>
2023-02-11 16:47:38 +0530

    all user routes done

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,93 +1,182 @@
-const Product = require('../models/productModel');
+const Product = require("../models/productModel");
 const ErrorHandler = require("../utils/errorhandler");
-const catchAsyncErrors = require('../middleware/catchAsyncErrors');
-const ApiFeatures = require('../utils/apifeatures');
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apifeatures");
 
 // Create Product -- ADMIN
 
 exports.createProduct = catchAsyncErrors(async (req, res, next) => {
+  req.body.user = req.user.id;
+  const product = await Product.create(req.body);
 
-    req.body.user = req.user.id;
-    const product = await Product.create(req.body);
- 
-    res.status(201).json({
-        sucess: true,
-        product
-    })
-})
+  res.status(201).json({
+    success: true,
+    product,
+  });
+});
 
 // Get All Product
 exports.getAllProducts = catchAsyncErrors(async (req, res) => {
+  const resultPerPage = 5;
+  const productCount = await Product.countDocuments();
 
-    const resultPerPage = 5;
-    const productCount = await Product.countDocuments();
-
-    const apiFeature = new ApiFeatures(Product.find(), req.query)
-        .search()
-        .filter()
-        .pagination(resultPerPage)
-    const products = await apiFeature.query;
-    res.status(200).json({
-        sucess: true,
-        products
-    })
-
-})
+  const apiFeature = new ApiFeatures(Product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(resultPerPage);
+  const products = await apiFeature.query;
+  res.status(200).json({
+    success: true,
+    products,
+  });
+});
 
 // Get Product Details
 
 exports.getProductDetails = catchAsyncErrors(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
 
-    const product = await Product.findById(req.params.id);
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
 
-    if (!product) {
-        return next(new ErrorHandler("Product not found", 404))
-    }
-
-    res.status(200).json({
-        sucess: true,
-        product,
-        productCount
-    })
-})
-
+  res.status(200).json({
+    success: true,
+    product,
+    // productCount,
+  });
+});
 
 // Update Product --ADMIN
 
 exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
 
-    let product = await Product.findById(req.params.id);
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
 
-    if (!product) {
-        return next(new ErrorHandler("Product not found", 404))
-    }
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
 
-    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true,
-        useFindAndModify: false
-    })
-
-    res.status(200).json({
-        sucess: true,
-        product
-    })
-})
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
 
 // Delete Product --ADMIN
 
 exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
-    const product = await Product.findById(req.params.id);
+  const product = await Product.findById(req.params.id);
 
-    if (!product) {
-        return next(new ErrorHandler("Product not found", 404))
-    }
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
 
-    await product.remove();
+  await product.remove();
 
-    res.status(200).json({
-        sucess: true,
-        message: "Product Delete Successfully"
-    })
+  res.status(200).json({
+    success: true,
+    message: "Product Delete Successfully",
+  });
 });
 
+// Create New Review or Update Review
+
+exports.createProductReview = catchAsyncErrors(async (req, res, next) => {
+  const { rating, comment, productId } = req.body;
+  const review = {
+    user: req.user._id,
+    name: req.user.name,
+    rating: Number(rating),
+    comment,
+  };
+
+  const product = await Product.findById(productId);
+
+  const isReviewed = product.reviews.find(
+    (rev) => rev.user.toString() === req.user._id.toString()
+  );
+
+  if (isReviewed) {
+    product.reviews.forEach((rev) => {
+      if (rev.user.toString() === req.user._id.toString())
+        (rev.rating = rating), (rev.comment = comment);
+    });
+  } else {
+    product.reviews.push(review);
+    product.numOfReviews = product.reviews.length;
+  }
+
+  let avg = 0;
+  product.reviews.forEach((rev) => {
+    avg += rev.rating;
+  });
+  product.ratings = avg / product.reviews.length;
+
+  await product.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+//   Get all reviews of a product
+
+exports.getProductReviews = catchAsyncErrors(async (req, res, next) => {
+  const product = await Product.findById(req.query.id);
+
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    reviews: product.reviews,
+  });
+});
+
+// Delete Review
+
+exports.deleteReview = catchAsyncErrors(async (req, res, next) => {
+  const product = await Product.findById(req.query.productId);
+
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
+
+  const reviews = product.reviews.filter(
+    (rev) => rev._id.toString() !== req.query.id.toString()
+  );
+
+  let avg = 0;
+
+  reviews.forEach((rev) => {
+    avg += rev.rating;
+  });
+  const ratings = avg / reviews.length;
+
+  const numOfReviews = reviews.length;
+
+  await Product.findByIdAndUpdate(
+    req.query.productId,
+    {
+      reviews,
+      ratings,
+      numOfReviews,
+    },
+    {
+      new: true,
+      runValidators: true,
+      userFindAndModify: false,
+    }
+  );
+
+  res.status(200).json({
+    success: true,
+  });
+});
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -136,3 +136,129 @@ exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
 
   sendToken(user, 200, res);
 });
+
+// Get User Details
+
+exports.getUserDetails = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
+// Update User Password
+
+exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select("+password");
+
+  const isPasswordMatched = await user.comparePassword(req.body.oldPassword);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Old password is incorrect", 400));
+  }
+
+  if (req.body.newPassword !== req.body.confirmPassword) {
+    return next(new ErrorHandler("Confirm Password does not match", 400));
+  }
+
+  user.password = req.body.newPassword;
+
+  await user.save();
+
+  sendToken(user, 200, res);
+});
+
+// Update User Profile
+
+exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
+
+  const newUserData = {
+    name:req.body.name,
+    email:req.body.email
+  }
+
+  // we will add Cloudinary later
+
+  const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+    new:true,
+    runValidators:true,
+    useFindAndModify:false,
+  })
+
+  res.status(200).json({
+    success: true,
+  });
+
+});
+
+// Get All Users --ADMIN
+
+exports.getAllUsers = catchAsyncErrors(async (req, res, next) => {
+
+  const users = await User.find();
+
+  res.status(200).json({
+    success:true,
+    users,  
+  })
+
+})
+
+// Get Single User Details --ADMIN
+
+exports.getSingleUser = catchAsyncErrors(async (req, res, next) => {
+
+  const user = await User.findById(req.params.id);
+
+  if(!user){
+    return next(new ErrorHandler(`User does not exist with id ${req.params.id}`))
+  }
+  res.status(200).json({
+    success:true,
+    user,  
+  })
+
+})
+
+// Update User Role --ADMIN
+exports.updateUserRole = catchAsyncErrors(async (req, res, next) => {
+
+  const newUserData = {
+    name:req.body.name,
+    email:req.body.email,
+    role:req.body.role,
+  }
+
+  const user = await User.findByIdAndUpdate(req.params.id, newUserData, {
+    new:true,
+    runValidators:true,
+    useFindAndModify:false,
+  })
+
+  res.status(200).json({
+    success: true,
+  });
+
+});
+
+// Delete User --ADMIN
+
+exports.deleteUser = catchAsyncErrors(async (req, res, next) => {
+
+  const user = await User.findByIdAndUpdate(req.params.id);
+
+  if(!user){
+    return next(new ErrorHandler(`User does not exist with id: ${req.params.id}`))
+  }
+
+  await user.remove();
+
+  res.status(200).json({
+    success: true,
+    message:"User Deleted Successfully"
+  });
+
+});
+
